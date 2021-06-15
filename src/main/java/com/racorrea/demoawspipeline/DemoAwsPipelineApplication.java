61411be20dd1e297a62ad68b2526aa775c432cb4
Roddy Andres Correa <ra.correa90@gmail.com>
2021-06-15 12:52:39 -0500

    first commit

---
+++ src/main/java/com/racorrea/demoawspipeline/DemoAwsPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.racorrea.demoawspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAwsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAwsPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/racorrea/demoawspipeline/DemoAwsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.racorrea.demoawspipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAwsPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
